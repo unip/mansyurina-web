f6c959d8d99be0eb28c87967470baeed68da70f8
Hanif <abdurrahman.alhanif@gmail.com>
2022-05-22 23:41:29 +0700

    add sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         "green-baby": "#B3D968",
         peach: "#F6C346",
         "dark-red": "#7C3939",
+        grey: "#EBEBEB",
+        "dark-grey": "#A1A1A1",
       },
     },
   },
